5ae0e641f24e36c59bc5a0aa81d5e4492ab56798
Kate Yu <katexyu@gmail.com>
2015-07-04 10:06:21 -0700

    Add favicon

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.set('view engine', 'jade');
 
 db = mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/stalkmyrecruit");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

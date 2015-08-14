0ab7b4631b79a12d22946a771b94e854953b7de8
Kate Yu <katexyu@gmail.com>
2015-08-13 20:42:46 -0700

    Cleanup unused function

--- client/compare.js
+++ client/compare.js
@@ -83,29 +83,6 @@ function getNewComparison(recruits, roster){
   });
 }
 
-/* Source: https://twitter.github.io/typeahead.js/examples/ */
-function substringMatcher(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
-
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
 $(document).ready(function(){
   var recruits = [];
   var roster = [];
